6da1c1f265c3055981318b58947611a27cc4225a
TOPERSON <maicon.silva11@fatec.sp.gov.br>
2022-02-23 11:02:45 -0300

    first commit , arrumar produtos nomes..

---
+++ ProjectFatec/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectFatec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFatec.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public static List<Produto> lsProdutos = new List<Produto>();
+        public IActionResult Index()
+        {
+            return View(lsProdutos);
+        }
+
+        public IActionResult Produtos()
+        {
+            return View();
+        }
+        public IActionResult salvar(Produto p)
+        {
+            lsProdutos.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjectFatec/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFatec.Models
+{
+    public class Produto
+    {
+        public string nome { get; set; }
+        public string descricao { get; set; }
+    }
+}
